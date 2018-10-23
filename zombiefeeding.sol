5e6be9dfcd930b6aaa5d4794913e91e65558b002
Eric Boutueil <e.boutueil@gmail.com>
2018-10-23 11:19:53 +0200

    level3

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -19,24 +19,27 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
 
-  // modifiez la définition de la fonction ici :
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    // ajoutez une déclaration `if` ici
+    if (keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
+    }
     _createZombie("NoName", newDna);
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    // et modifiez l'appel de la fonction ici :
-    feedAndMultiply(_zombieId, kittyDna);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
 }
